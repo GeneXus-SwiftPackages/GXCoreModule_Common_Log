dbb971123a9f265e870540f0d011968c40f3ef24
genexusbot <builder@genexus.com>
2023-03-27 17:07:39 -0300

    Package version: 1.0.0-beta+20230327200102

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
 				dependencies: [
 					"GXCoreModule_Common_Log",
 					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .watchOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS, .watchOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .watchOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_Common_Log",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_Log-1.0.0-beta+20230324153906.xcframework.zip",
-			checksum: "d382fe9f13abff3115957040e7ca7853d9db933e735e15c3de406e433b91a87d"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_Log-1.0.0-beta+20230327200102.xcframework.zip",
+			checksum: "18905ec31966d84ed00c6ba2aa75ca462119d096806ce5d5302dc9a8589ca967"
 		)
 	]
 )
