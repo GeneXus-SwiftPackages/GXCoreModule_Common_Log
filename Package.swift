93f715c7847662df5e9cb82f55410eb21076d3b4
Build Er <builder@macmini6.local>
2023-05-03 10:24:00 -0300

    Package version: 1.0.0-beta+20230503131704

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
 		.target(name: "GXCoreModule_Common_LogWrapper",
 				dependencies: [
 					"GXCoreModule_Common_Log",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .watchOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .watchOS, .iOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.watchOS, .tvOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_Common_Log",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_Log-1.0.0-beta+20230502131235.xcframework.zip",
-			checksum: "c9d3f068e1d7c24f0583b1798fe20b567c95db1f6677b830664ada8f8ccf800e"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_Log-1.0.0-beta+20230503131704.xcframework.zip",
+			checksum: "9fdf4f021ac64de4e742bb877d947001b91fb6a3ee2a0c0e3c2b245fe2ebddba"
 		)
 	]
 )
