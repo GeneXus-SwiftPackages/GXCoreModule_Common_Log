4849a967c0073c8aa54ba65a1a088edd5dd83ede
Build Er <builder@macmini6.local>
2023-12-07 15:24:13 -0300

    Package version: 1.3.0

--- Package.swift
+++ Package.swift
@@ -10,21 +10,19 @@ let package = Package(
 			targets: ["GXCoreModule_Common_LogWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.3.0")
 	],
 	targets: [
 		.target(name: "GXCoreModule_Common_LogWrapper",
 				dependencies: [
 					"GXCoreModule_Common_Log",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.watchOS, .tvOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.watchOS, .iOS, .tvOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_Common_Log",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_Common_Log-1.1.0.xcframework.zip",
-			checksum: "abf896e2d27ef6a486442007aa39c0cfad30d742f66ce6f43440200847162566"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_Common_Log-1.3.0.xcframework.zip",
+			checksum: "dfe7e187e24aa13b82865cafd4b471a43992599554e4a8c6d60941a585311fb5"
 		)
 	]
 )
