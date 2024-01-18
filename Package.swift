f39b0a7dcfbbca623b08d0fe017f689a11cccfb4
genexusbot <builder@genexus.com>
2024-01-18 00:39:23 -0300

    Package version: 1.4.0-rc.21

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
 	name: "GXCoreModule_Common_Log",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0")],
 	products: [
 		.library(
 			name: "GXCoreModule_Common_Log",
 			targets: ["GXCoreModule_Common_LogWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.0-rc.10")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.0-rc.21")
 	],
 	targets: [
 		.target(name: "GXCoreModule_Common_LogWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_Common_Log",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXCoreModule_Common_Log-1.4.0-rc.10.xcframework.zip",
-			checksum: "074d7f57d4be511d7f1d939e3b6c607738dfc9dcdb2f4f55cb34060d45c57ce2"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXCoreModule_Common_Log-1.4.0-rc.21.xcframework.zip",
+			checksum: "3b7e75a4da3198b2ca4963373ff38c13a6ffd50e0345e77b979a7d799ee8c534"
 		)
 	]
 )
