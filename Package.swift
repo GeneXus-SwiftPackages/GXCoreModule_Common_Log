7e902b2252fd0e12e3d326d0d0edb01082c1045d
genexusbot <builder@genexus.com>
2024-02-09 16:14:11 -0300

    Package version: 1.5.0-rc.2

--- Package.swift
+++ Package.swift
@@ -3,28 +3,26 @@ import PackageDescription
 
 let package = Package(
 	name: "GXCoreModule_Common_Log",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0")],
 	products: [
 		.library(
 			name: "GXCoreModule_Common_Log",
 			targets: ["GXCoreModule_Common_LogWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.5.0-rc.2")
 	],
 	targets: [
 		.target(name: "GXCoreModule_Common_LogWrapper",
 				dependencies: [
 					"GXCoreModule_Common_Log",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.watchOS, .tvOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.watchOS, .iOS, .tvOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_Common_Log",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_Common_Log-1.1.0.xcframework.zip",
-			checksum: "abf896e2d27ef6a486442007aa39c0cfad30d742f66ce6f43440200847162566"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXCoreModule_Common_Log-1.5.0-rc.2.xcframework.zip",
+			checksum: "698981f6d1677a125539ca52532806340a876ab37e251f3aca66b2149ce7c620"
 		)
 	]
 )
