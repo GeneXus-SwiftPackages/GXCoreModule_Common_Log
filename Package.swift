4055c9297601158e5307b8724e02a95a2c313b13
The iOS Team @ GeneXus <builder@genexus.com>
2026-03-31 15:52:59 -0400

    Package version: 4.2.0-beta.2

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_Common_LogWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.2.0-beta.1")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.2.0-beta.2")
 	],
 	targets: [
 		.target(name: "GXCoreModule_Common_LogWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_Common_Log",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_Log-4.2.0-beta.1.xcframework.zip",
-			checksum: "a831bd1109527b6979e4c3d5de3ae1afa8eba08d392fa435f14950ba1be4c55d"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_Log-4.2.0-beta.2.xcframework.zip",
+			checksum: "89ec2cf581bed22dc1332565c3b6decc8e3f726e4c8e2cddf0426d7914e4a174"
 		)
 	]
 )
